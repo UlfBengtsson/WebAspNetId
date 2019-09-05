7c2f945cadd2e6fbefbbce2175fcefc039cd1d6b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-09-05 09:33:39 +0200

    Add project files.

---
+++ WebAspNetId/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAspNetId.Models.Id;
+
+namespace WebAspNetId.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserViewModel newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser() { Email = newUser.Email, UserName = newUser.UserName, Age = newUser.Age };
+                IdentityResult result = await userManager.CreateAsync(appUser, newUser.Password);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(newUser);
+        }
+    }
+}
---
+++ WebAspNetId/Models/Id/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAspNetId.Models.Id
+{
+    public class UserViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
